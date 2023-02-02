77753b1fc0d3381e1ac7c0b8f6a2928783ef3ba3
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-02-02 16:48:27 +0000

    level 100 done

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const pCounter = document.querySelector("#counter");
+  pCounter.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach(eachEpisode => {
+
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("container");
+
+    const pName = document.createElement("p");
+    const seasonNumber = eachEpisode.season.toString().padStart(2, "0");
+    const episodeNumber = eachEpisode.number.toString().padStart(2, "0");
+    pName.textContent = `${eachEpisode.name} - S${seasonNumber}E${episodeNumber}`;
+    pName.classList.add("title");
+
+    const img = document.createElement("img");
+    img.src = eachEpisode.image.medium;
+
+    rootElem.appendChild(divContainer);
+    divContainer.appendChild(pName);
+    divContainer.appendChild(img);
+    divContainer.innerHTML += eachEpisode.summary;
+  });
 }
 
 window.onload = setup;
