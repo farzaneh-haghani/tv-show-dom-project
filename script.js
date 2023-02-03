127eec9b214b6c5a72d523f3179ca2c595f88ac2
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-02-03 20:48:09 +0000

    level 200 done

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 //You can edit ALL of the code here
+
+const rootElem = document.getElementById("root");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  const inputSearch = document.querySelector("#liveSearch");
+  inputSearch.addEventListener("keyup", () => {
+    const newEpisodesArr = [];
+    rootElem.innerHTML = "";
+    allEpisodes.forEach(eachEpisode => {
+      if (eachEpisode.name.toLowerCase().includes(inputSearch.value.toLowerCase()) ||
+        eachEpisode.summary.toLowerCase().includes(inputSearch.value.toLowerCase()))
+        newEpisodesArr.push(eachEpisode);
+    })
+    makePageForEpisodes(newEpisodesArr);
+  })
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
   const pCounter = document.querySelector("#counter");
-  pCounter.textContent = `Got ${episodeList.length} episode(s)`;
-
+  pCounter.textContent = `Displaying ${episodeList.length} / episode(s)`;
   episodeList.forEach(eachEpisode => {
-
-    const divContainer = document.createElement("div");
-    divContainer.classList.add("container");
-
-    const pName = document.createElement("p");
-    const seasonNumber = eachEpisode.season.toString().padStart(2, "0");
-    const episodeNumber = eachEpisode.number.toString().padStart(2, "0");
-    pName.textContent = `${eachEpisode.name} - S${seasonNumber}E${episodeNumber}`;
-    pName.classList.add("title");
-
-    const img = document.createElement("img");
-    img.src = eachEpisode.image.medium;
-
-    rootElem.appendChild(divContainer);
-    divContainer.appendChild(pName);
-    divContainer.appendChild(img);
-    divContainer.innerHTML += eachEpisode.summary;
+    showEachEpisode(eachEpisode);
   });
 }
+
+
+
+function showEachEpisode(episode) {
+  const divContainer = document.createElement("div");
+  divContainer.classList.add("container");
+
+  const pName = document.createElement("p");
+  const seasonNumber = episode.season.toString().padStart(2, "0");
+  const episodeNumber = episode.number.toString().padStart(2, "0");
+  pName.textContent = `${episode.name} - S${seasonNumber}E${episodeNumber}`;
+  pName.classList.add("title");
+
+  const img = document.createElement("img");
+  img.src = episode.image.medium;
+
+  rootElem.appendChild(divContainer);
+  divContainer.appendChild(pName);
+  divContainer.appendChild(img);
+  divContainer.innerHTML += episode.summary;
+}
+
 
 window.onload = setup;
