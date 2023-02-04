71757a390f15ea1a469a336220278afa41567797
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-02-04 04:31:26 +0000

    level 300 done

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
 //You can edit ALL of the code here
 
 const rootElem = document.getElementById("root");
+const inputSearch = document.querySelector("#liveSearch");
+const pCounter = document.querySelector("#counter");
+const selectMovie = document.querySelector("#selectMovie");
+
 
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-  const inputSearch = document.querySelector("#liveSearch");
-  inputSearch.addEventListener("keyup", (event) => {
-    const newEpisodesArr = [];
+  makeSelectAndOptions(allEpisodes);      //Level 300
+  makePageForEpisodes(allEpisodes);       //Level 200
+  makeSearch(allEpisodes);                //Level 100
+}
+
+
+function makeSelectAndOptions(episodeList) {
+  episodeList.forEach(eachEpisode => {
+    const option = document.createElement("option");
+    const seasonNumber = eachEpisode.season.toString().padStart(2, "0");
+    const episodeNumber = eachEpisode.number.toString().padStart(2, "0");
+    option.innerText = `S${seasonNumber}E${episodeNumber} - ${eachEpisode.name}`
+    selectMovie.appendChild(option);
+  })
+  selectMovie.addEventListener("change", (event) => {
     rootElem.innerHTML = "";
-    allEpisodes.forEach(eachEpisode => {
-      if (eachEpisode.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
-        eachEpisode.summary.toLowerCase().includes(event.target.value.toLowerCase()))
-        newEpisodesArr.push(eachEpisode);
-    })
-    makePageForEpisodes(newEpisodesArr);
+    if (event.target.value === "all")
+      makePageForEpisodes(episodeList);
+    else {
+      const newEpisodesList = episodeList.filter(eachEpisode =>
+        eachEpisode.name.includes(event.target.value.split(" - ")[1]));
+      makePageForEpisodes(newEpisodesList);
+    }
   })
 }
 
+
 function makePageForEpisodes(episodeList) {
-  const pCounter = document.querySelector("#counter");
   pCounter.textContent = `Displaying ${episodeList.length} / 73 episode(s)`;
   episodeList.forEach(eachEpisode => {
-    showEachEpisode(eachEpisode);
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("container");
+    const pName = document.createElement("p");
+    const seasonNumber = eachEpisode.season.toString().padStart(2, "0");
+    const episodeNumber = eachEpisode.number.toString().padStart(2, "0");
+    pName.textContent = `${eachEpisode.name} - S${seasonNumber}E${episodeNumber}`;
+    pName.classList.add("title");
+    const img = document.createElement("img");
+    img.src = eachEpisode.image.medium;
+    rootElem.appendChild(divContainer);
+    divContainer.appendChild(pName);
+    divContainer.appendChild(img);
+    divContainer.innerHTML += eachEpisode.summary;
   });
 }
 
 
-
-function showEachEpisode(episode) {
-  const divContainer = document.createElement("div");
-  divContainer.classList.add("container");
-
-  const pName = document.createElement("p");
-  const seasonNumber = episode.season.toString().padStart(2, "0");
-  const episodeNumber = episode.number.toString().padStart(2, "0");
-  pName.textContent = `${episode.name} - S${seasonNumber}E${episodeNumber}`;
-  pName.classList.add("title");
-
-  const img = document.createElement("img");
-  img.src = episode.image.medium;
-
-  rootElem.appendChild(divContainer);
-  divContainer.appendChild(pName);
-  divContainer.appendChild(img);
-  divContainer.innerHTML += episode.summary;
+function makeSearch(episodeList) {
+  inputSearch.addEventListener("keyup", (event) => {
+    rootElem.innerHTML = "";
+    const newEpisodesList = episodeList.filter(eachEpisode =>
+      eachEpisode.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
+      eachEpisode.summary.toLowerCase().includes(event.target.value.toLowerCase()))
+    makePageForEpisodes(newEpisodesList);
+  })
 }
-
 
 window.onload = setup;
